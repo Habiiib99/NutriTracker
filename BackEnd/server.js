8b40b6822202a2e57c1427cf61c3ea73a4ada0d1
kennethblicher <167316502+kennethblicher@users.noreply.github.com>
2024-05-08 15:06:50 +0200

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -87,10 +87,11 @@ async function fetchFoodID(searchString) {
       },
     });
     if (response.ok) {
+      // Behandler svaret fra API'en.
       const result = await response.json();
       // Sender det første foodID, hvis GET-anmodningen lykkes. Spørgsmål. Skulle dette ikke være så man kan vælge mellem alle mulighederne?
       return result[0].foodID;
-    // Hvis anmodningen ikke lykkes, logges fejl til konsollen med fejlmeddelelse med response-status.
+    // Hvis GET-anmodningen ikke lykkes, logges fejl til konsollen med fejlmeddelelse med response-status.
     } else {
       console.error('Failed to fetch data. Status:', response.status);
       return null;
@@ -104,8 +105,10 @@ async function fetchFoodID(searchString) {
 
 // Funktion til at hente næringsværdier baseret på foodID og sortKey
 async function fetchNutrientValue(foodID, sortKey) {
+  // URL baseret på foodID og sortKey
   const url = `https://nutrimonapi.azurewebsites.net/api/FoodCompSpecs/ByItem/${foodID}/BySortKey/${sortKey}`;
   try {
+    // GET-anmodning til at hente næringsværdierne fra API'en.
     const response = await fetch(url, {
       method: 'GET',
       headers: {
@@ -113,18 +116,23 @@ async function fetchNutrientValue(foodID, sortKey) {
         'X-API-Key': apiKey,
       },
     });
+    // Behandler svaret fra API'en.
     if (response.ok) {
       const result = await response.json();
+      // Hvis anmodning lykkes, returneres næringsværdierne for foodID'et og sortKey.
       if (result.length > 0) {
         return result[0].resVal;
+      // Hvis der ikke er næringsværdier i API'en for det angivne foodID, logges fejlmeddelelse om dette.
       } else {
         console.log(`Nutrient value not found for foodID: ${foodID}`);
         return null;
       }
+     // Hvis GET-anmodningen ikke lykkes, logges fejl til konsollen med fejlmeddelelse med response-status.
     } else {
       console.error('Failed to fetch nutrient value. Status:', response.status);
       return null;
     }
+    // Hvis der sker en fejl i løbet af anmodningen, logges fejl til konsollen med fejl ved at fetche data.
   } catch (error) {
     console.error('Error fetching nutrient value:', error);
     return null;
